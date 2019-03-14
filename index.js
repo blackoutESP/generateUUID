c07563d93d1126a3e330bf725fc4e4250e98d7ad
blackoutESP <yxion10101@gmail.com>
2019-03-14 09:11:02 +0100

    .

---
+++ index.js
@@ -0,0 +1,21 @@
+let area = document.querySelector('#uuid');
+let generateButton = document.querySelector('#generate');
+
+generateButton.addEventListener('click', (e)=>{
+    e.preventDefault();
+    area.innerHTML = '';
+    console.log(generateUUID());
+    let uuid = generateUUID();
+    area.innerHTML = uuid;
+});
+
+const generateUUID = ()=>{
+    // 'xxxxxxxx-xxxx-4xxxxxxx-yxxx-xxxxxxxx'
+    let dateTime = new Date().getTime();
+    let uuid = '2xxxxxxx-0xxx-4xxxxxxx-yxxx-1xxxxxxx'.replace(/[xy]/g, (v)=>{
+        let reg = (dateTime + Math.random()*32)%32 | 0;
+        dateTime = Math.floor(dateTime/32);
+        return (v==='x' ? reg: (reg&0x2|0x3|0x5|0x8).toString(32));
+    });
+    return uuid;
+};
